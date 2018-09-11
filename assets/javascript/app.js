3a01e7b7abc8631926f469e36537044b26a13350
Jacqueline Gaherity <v-jagahe@microsoft.com>
2018-09-11 15:08:00 -0700

    logic re worked for click events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,25 +5,107 @@
 //when html loads, execute function
 $(document).ready(function() {
 
-    var correct = 2;
+    var trivia = {
+        first: {
+            question: '1. Created in 2009, what was the first decentralized cryptocurrency?',
+            answers: {
+                a: 'Z-Cash', 
+                b: 'LiteCoin',
+                c: 'Bitcoin',
+                d: 'Bitcoin cash'
+            },
+            correctAnswers: {
+                a: 'Bitcoin',
+            }
+        },
+        second: {
+            question: '2. Created in 2009, what was the first decentralized cryptocurrency?',
+            answers: {
+                a: 'Z-Cash',
+                b: 'LiteCoin',
+                c: 'Bitcoin',
+                d: 'Bitcoin cash'
+            },
+            correctAnswers: {
+                a: 'Bitcoin',
+            }
+        },
+        third: {
+            question: '3. Created in 2009, what was the first decentralized cryptocurrency?',
+            answers: {
+                a: 'Z-Cash',
+                b: 'LiteCoin',
+                c: 'Bitcoin',
+                d: 'Bitcoin cash'
+            },
+            correctAnswers: {
+                a: 'Bitcoin',
+            }
+        }
+    }
 
+
+    var correct = 0;
+    //all correct possible answers
     //jquery click functions for each correct answer 
-    $( "#correct" ).click(function() {
-        //console.log("First correct answer selected");
-        correct--;
-        console.log(correct);
-    });
+    // $( ".answer" ).click(function() {
+    //     if(this.id == "correct"){
+    //         correct++;
+    //         alert(correct);
+    //         console.log(correct);
+    //     }
+    // });
+
+    $("#question1").text(trivia.first.question);
+    $("#question2").text(trivia.second.question);
+    $("#question3").text(trivia.third.question);
+
+    $("#1answera").text(trivia.first.answers.a);
+    $("#1answerb").text(trivia.first.answers.b);
+    $("#1answerc").text(trivia.first.answers.c);
+    $("#1answerd").text(trivia.first.answers.d);
+
+    //IF selected answer == trivia.first.correctAnswers.a, correct++
+    //capture value of click
+    $(".selected1").on("click", function() {
+        //alert("Selected thing");
+        var temp = $(this).html();
+        //console.log(temp);
+        if(trivia.first.correctAnswers.a == temp){
+            console.log("right");
+            correct++;
+        } else{
+            console.log("wrong");
+        }
+        //console.log("Correct answer: " + trivia.first.correctAnswers.a);
+        //console.log("Guessed answer: " + temp);
+        //console.log(temp);
+
+      });
+
+
+    
+    $("#2answera").text(trivia.second.answers.a);
+    $("#2answerb").text(trivia.second.answers.b);
+    $("#2answerc").text(trivia.second.answers.c);
+    $("#2answerd").text(trivia.second.answers.d);
+
+    //IF selected answer == trivia.first.correctAnswers.a, correct++
+    $("#3answera").text(trivia.third.answers.a);
+    $("#3answerb").text(trivia.third.answers.b);
+    $("#3answerc").text(trivia.third.answers.c);
+    $("#3answerd").text(trivia.third.answers.d);
+
+    
+
 
     var windowTimeout = function(){
-        console.log()
+        //console.log()
         setTimeout(function() {
-        alert("Add element.");
-        $( ".modal" ).addClass("overlay");
-        $(".backgroundImage").append($("<div class='modal'>"));
-      }, 1000);
+        alert("You ran out of time. Your score is: " + correct);
+        //$(".backgroundImage").append($("<div class='modal'>"));
+      }, 2000);
     }
-    windowTimeout()
-
-
+    windowTimeout();
 
 });
